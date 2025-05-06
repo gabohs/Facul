63065172df613fec0940d7c6e82546b06c1fdca2
gabohs <gabinho_schaeff@outlook.com>
2025-05-06 17:12:46 -0300

    Movendo os exercicios para um unico projeto

--- 1sem/Algo 1/AULAS/Aula5_ERepeticao/src/Exercicio1.java
+++ 1sem/Algo 1/AULAS/Aula5_ERepeticao/src/Exercicio1.java
@@ -5,7 +5,8 @@ Numeros pares de 1 a 1000
 public class Exercicio1
 {
     public static void main(String[] args)
-    {
+    {   
+        
         for (int n = 1; n <= 1000; n++)
         {
             if (n % 2 == 0) System.out.println(n);
@@ -15,6 +16,7 @@ public class Exercicio1
         {
             System.out.println(n);
         }
+        
     }
     
 }
