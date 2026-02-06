aa05e3568ce459c5e7958182e7278abca7979ad7
gabohs <schaeffer.gabrielh@gmail.com>
2026-02-06 11:29:26 -0300

    Organizando...

--- 1sem/Algo 1/TRABALHOS/Lista_Modularizacao/src/Exercicio05.java
+++ 1sem/Algo 1/TRABALHOS/Lista_Modularizacao/src/Exercicio05.java
@@ -14,10 +14,7 @@ public class Exercicio05
 {   
     public static boolean ehPar(int numero)
     {
-        if (numero % 2 == 0)
-            return true;
-        
-        return false;
+        return numero % 2 == 0;
     }
     
     public static void main(String[] args)
